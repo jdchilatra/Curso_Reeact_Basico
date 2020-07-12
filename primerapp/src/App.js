ad9153449ac1adf3cd908d90c8819bd9963f7861
David <davidchilatra@gmail.com>
2020-07-12 10:12:25 -0500

    Se definen los componentes como clases y se define el estado del componente

--- primerapp/src/App.js
+++ primerapp/src/App.js
@@ -1,13 +1,42 @@
 import React from "react"
 import "./App.css"
 
-function Helloworld(props){ //Componentes estaticos como funciones
-    return(
-    <div>
-        <div id="hello">Hello world  {props.mytext}</div>
-    <div>{props.subtitle}</div>
-    </div>
-    )
+// function Helloworld(props){ //Componentes estaticos como funciones
+//     return(
+//     <div>
+//         <div id="hello">Hello world  {props.mytext}</div>
+//     <div>{props.subtitle}</div>
+//     </div>
+//     )
+// }
+
+class Helloworld extends React.Component{ //clase helloworld que hereda  las propiedades de un componente
+    state={ //
+        show:true
+    }
+    toggle=() =>{
+            this.setState({show:!this.state.show})
+
+    }
+    render(){
+        if(this.state.show){
+            return(
+                <div>
+                    <div id="hello">Hello world  {this.props.mytext}</div>
+                    <div>{this.props.subtitle}</div>
+                    <button onClick={this.toggle}>toggle show</button>
+                </div>
+                )
+        }else{
+            return(
+                <div>
+                    <h1>No hay elementos</h1>
+                    <button onClick={this.toggle}>toggle show</button>
+                </div>
+            )
+        }
+        
+    }
 }
 
 function App(){
